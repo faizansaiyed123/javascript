57cd3e8e58efa9a5abc10a57047f3c51787df718
faizan saiyed <fai1ggj@gmail.com>
2025-07-01 20:11:59 +0530

    strings

---
+++ string.js
@@ -0,0 +1,13 @@
+const fname = "Faizan";
+const repoCount = 23;
+
+// console.log(`Hello my name is ${fname} and my repocount is ${repoCount}`)
+// console.log(fname.charAt(2))
+// console.log(fname.indexOf("z"))
+// console.log(fname.toUpperCase())
+
+// let iname = fname.substring(0,3)
+// console.log(iname)
+
+const anotherString = fname.slice(0,4)
+console.log(anotherString)
